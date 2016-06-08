1b56e98882f22a52fb6bfac4ffb068de91fe481b
Evan Zarowitz <evzaro@gmail.com>
2016-06-07 20:14:03 -0400

    working with images

--- app/views/api/nap_spots/index.json.jbuilder
+++ app/views/api/nap_spots/index.json.jbuilder
@@ -3,6 +3,7 @@ json.nap_spots @nap_spots do |nap_spot|
   json.owner_id  nap_spot.owner_id
   json.price number_to_currency nap_spot.price
   json.title nap_spot.title
+  json.category nap_spot.category
   json.image_url nap_spot.image_urls
   json.latlng JSON.parse(nap_spot.location)['latlng']
 
--- frontend/components/spot_index_item.jsx
+++ frontend/components/spot_index_item.jsx
@@ -4,18 +4,18 @@ var React = require('react');
 var SpotIndexItem = React.createClass({
 
   //markers
+  //title length limit
 
   render: function() {
-
     return (
       <div className="index-item-container">
         <div className="index-item-content">
           <img className="index-item-img" src={this.props.spot.image_url}/>
           <span>{this.props.spot.price}</span>
           <div></div>
-          <h2 className="index-title">{this.props.spot.title}</h2>
-          <h3 className="index-cat">{this.props.spot.category}</h3>
         </div>
+        <h2 className="index-title">{this.props.spot.title}</h2>
+        <h3 className="index-cat">{this.props.spot.category}</h3>
       </div>
     );
   }
