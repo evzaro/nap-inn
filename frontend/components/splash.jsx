3d2802432f99f9839a7e001d56660f1951ac5abd
evzaro <evzaro@gmail.com>
2016-06-30 16:01:05 -0400

    started featured

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -1,8 +1,33 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
 var Search = require('./search');
+var MySpotItem = require('./my_spot_item');
+var NapSpotApiUtil = require('../util/nap_spot_api_util');
+var NapSpotStore = require('../stores/nap_spot_store');
 
 var Splash = React.createClass({
+
+  // getInitialState: function() {
+  //   return {
+  //     spot: NapSpotStore.currentSpot()
+  //     //nap_spots: NapSpotStore.all()
+  //   };
+  // },
+  //
+  // componentDidMount: function() {
+  //   NapSpotApiUtil.fetchNapSpot(1);
+  //   // RouteActions.changeRoute('index');
+  //   this.storeListener = NapSpotStore.addListener(this._handleChange);
+  //   // BookingApiUtil.fetchMyBookings();
+  //   // //1) fetch my bookings API and assocated nap spot id (my spot item)
+  //   // //2) add booking store listener
+  // },
+
+  _handleChange: function(){
+    this.setState({
+      spot: NapSpotStore.currentSpot()
+    });
+  },
 
   render: function() {
 
@@ -24,12 +49,10 @@ var Splash = React.createClass({
         <Search/>
         <div className="background" style={divStyle}>
           <h1 className="splash-header">{saying}</h1>
-
-      </div>
+        </div>
     </div>
     );
   }
-
 });
 
 module.exports = Splash;
