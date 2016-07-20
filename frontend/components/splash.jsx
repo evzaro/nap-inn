dd224a57d858c729e356b6106c3d53aeffa52ba8
evzaro <evzaro@gmail.com>
2016-07-20 14:09:57 -0400

    added links to cities

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -5,12 +5,24 @@ var MySpotItem = require('./my_spot_item');
 var NapSpotApiUtil = require('../util/nap_spot_api_util');
 var NapSpotStore = require('../stores/nap_spot_store');
 
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var hashHistory = ReactRouter.hashHistory;
+
 var Splash = React.createClass({
 
-  _handleChange: function(){
+  _handleChange: function() {
     this.setState({
       spot: NapSpotStore.currentSpot()
     });
+  },
+
+  goToDC: function() {
+    hashHistory.push('/index/38.9071923&-77.03687070000001');
+  },
+
+  goToNY: function() {
+    hashHistory.push('/index/40.7127837&-74.00594130000002');
   },
 
   render: function() {
@@ -45,8 +57,8 @@ var Splash = React.createClass({
           <h1 className="splash-header">{saying}</h1>
           <div className="splash-listings">
           <div className="featured clearfix">
-            <div id="ny"><h2>Washington, DC</h2><img src="http://res.cloudinary.com/dfjm0djds/image/upload/v1469031215/dcbutton_x7flfi.jpg" alt="DC" /></div>
-            <div id="dc"><h2>New York</h2><img src="http://res.cloudinary.com/dfjm0djds/image/upload/v1469031215/newyorkbutton_eyftpa.jpg" alt="NY" /></div>
+            <div id="ny" onClick={this.goToDC}><h2>Washington, DC</h2><img src="http://res.cloudinary.com/dfjm0djds/image/upload/v1469031215/dcbutton_x7flfi.jpg" alt="DC" /></div>
+            <div id="dc" onClick={this.goToNY}><h2>New York, NY</h2><img src="http://res.cloudinary.com/dfjm0djds/image/upload/v1469031215/newyorkbutton_eyftpa.jpg" alt="NY" /></div>
           </div>
           </div>
         </div>
